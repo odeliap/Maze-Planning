8b12fde1bbb9b8840d908483205af662fe63b6e4
Odelia Putterman <odeliaputterman@gmail.com>
2022-03-23 17:48:34 -0700

    change robot speed to complete maze!

--- lab3_robot.py
+++ lab3_robot.py
@@ -8,38 +8,39 @@ from lab3_planner import planner
 
 maze_map = {(1,1): {'E': 0, 'W': 0, 'N': 0, 'S': 0}, (1,2): {'E': 0, 'W': 0, 'N': 0, 'S': 0}, (1,3): {'E': 0, 'W': 0, 'N': 0, 'S': 0}, (1,4): {'E': 0, 'W': 0, 'N': 0, 'S': 0}, (1,5): {'E': 0, 'W': 0, 'N': 0, 'S': 1}, (1,6): {'E': 0, 'W': 0, 'N': 0, 'S': 0},
             (2,1): {'E': 0, 'W': 0, 'N': 0, 'S': 0}, (2,2): {'E': 0, 'W': 0, 'N': 0, 'S': 0}, (2,3): {'E': 0, 'W': 0, 'N': 0, 'S': 0}, (2,4): {'E': 0, 'W': 0, 'N': 0, 'S': 0}, (2,5): {'E': 0, 'W': 0, 'N': 1, 'S': 1}, (2,6): {'E': 0, 'W': 0, 'N': 0, 'S': 0},
-            (3,1): {'E': 1, 'W': 0, 'N': 0, 'S': 1}, (3,2): {'E': 1, 'W': 1, 'N': 0, 'S': 1}, (3,3): {'E': 1, 'W': 1, 'N': 0, 'S': 1}, (3,4): {'E': 1, 'W': 1, 'N': 0, 'S': 1}, (3,5): {'E': 0, 'W': 1, 'N': 1, 'S': 1}, (3,6): {'E': 0, 'W': 0, 'N': 0, 'S': 0},
-            (4,1): {'E': 1, 'W': 0, 'N': 1, 'S': 0}, (4,2): {'E': 1, 'W': 1, 'N': 1, 'S': 0}, (4,3): {'E': 1, 'W': 1, 'N': 1, 'S': 1}, (4,4): {'E': 1, 'W': 1, 'N': 1, 'S': 0}, (4,5): {'E': 0, 'W': 1, 'N': 1, 'S': 1}, (4,6): {'E': 0, 'W': 0, 'N': 0, 'S': 0},
-            (5,1): {'E': 0, 'W': 0, 'N': 0, 'S': 0}, (5,2): {'E': 0, 'W': 0, 'N': 0, 'S': 0}, (5,3): {'E': 0, 'W': 0, 'N': 1, 'S': 1}, (5,4): {'E': 0, 'W': 0, 'N': 0, 'S': 0}, (5,5): {'E': 0, 'W': 0, 'N': 1, 'S': 0}, (5,6): {'E': 0, 'W': 0, 'N': 0, 'S': 0},
-            (6,1): {'E': 0, 'W': 0, 'N': 0, 'S': 0}, (6,2): {'E': 0, 'W': 0, 'N': 0, 'S': 0}, (6,3): {'E': 0, 'W': 0, 'N': 1, 'S': 0}, (6,4): {'E': 0, 'W': 0, 'N': 0, 'S': 0}, (6,5): {'E': 0, 'W': 0, 'N': 0, 'S': 0}, (6,6): {'E': 0, 'W': 0, 'N': 0, 'S': 0}}
+            (3,1): {'E': 1, 'W': 0, 'N': 0, 'S': 0}, (3,2): {'E': 1, 'W': 1, 'N': 0, 'S': 0}, (3,3): {'E': 1, 'W': 1, 'N': 0, 'S': 1}, (3,4): {'E': 1, 'W': 1, 'N': 0, 'S': 0}, (3,5): {'E': 0, 'W': 1, 'N': 1, 'S': 1}, (3,6): {'E': 0, 'W': 0, 'N': 0, 'S': 0},
+            (4,1): {'E': 0, 'W': 0, 'N': 0, 'S': 0}, (4,2): {'E': 0, 'W': 0, 'N': 0, 'S': 0}, (4,3): {'E': 0, 'W': 0, 'N': 1, 'S': 1}, (4,4): {'E': 0, 'W': 0, 'N': 0, 'S': 0}, (4,4): {'E': 0, 'W': 0, 'N': 1, 'S': 0}, (4,6): {'E': 0, 'W': 0, 'N': 0, 'S': 0},
+            (5,1): {'E': 0, 'W': 0, 'N': 0, 'S': 0}, (5,2): {'E': 0, 'W': 0, 'N': 0, 'S': 0}, (5,3): {'E': 0, 'W': 0, 'N': 1, 'S': 0}, (5,4): {'E': 0, 'W': 0, 'N': 0, 'S': 0}, (5,5): {'E': 0, 'W': 0, 'N': 0, 'S': 0}, (5,5): {'E': 0, 'W': 0, 'N': 0, 'S': 0}}
 
 def go_straight(bot):
     bot.drive_direct(100, 100)
-    time.sleep(1.0)
+    time.sleep(2.7)
 
 def turn_right(bot):
-    bot.drive_direct(183, -183)
-    time.sleep(1.0)
-
-def turn_left(bot):
     bot.drive_direct(-183, 183)
     time.sleep(1.0)
 
+def turn_left(bot):
+    bot.drive_direct(183, -183)
+    time.sleep(1.0)
+
 if __name__ == "__main__":
-    port = '/dev/tty.usbserial-DN026A6A'
+    port = '/dev/tty.usbserial-DN0266RJ'
     # instantiate bot
     bot = Create2(port=port)
     # start bot in safe mode
     bot.start()
     bot.safe()
 
-    plan = planner(maze_map, 6, 3, 1, 5)
+    plan = planner(maze_map, 5, 3, 1, 5)
     print(plan)
 
     current_direction = 'N'
 
-    for task in plan[0]:
+    for action in plan:
+        task = action[0]
         if task == 'move_north':
+            print('moving north')
             if current_direction == 'N':
                 pass
             elif current_direction == 'E':
@@ -52,6 +53,7 @@ if __name__ == "__main__":
             go_straight(bot)
             current_direction = 'N'
         elif task == 'move_east':
+            print('moving east')
             if current_direction == 'N':
                 turn_right(bot)
             elif current_direction == 'E':
@@ -64,6 +66,7 @@ if __name__ == "__main__":
             go_straight(bot)
             current_direction = 'E'
         elif task == 'move_south':
+            print('moving south')
             if current_direction == 'N':
                 turn_right(bot)
                 turn_right(bot)
@@ -76,6 +79,7 @@ if __name__ == "__main__":
             go_straight(bot)
             current_direction = 'S'
         elif task == 'move_west':
+            print('moving west')
             if current_direction == 'N':
                 turn_left(bot)
             elif current_direction == 'E':
